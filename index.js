227d259fb3509a7ee0116d6a62d107921e782240
104vishnu <vishnubaraili1704025@gmail.com>
2024-03-15 12:48:12 +0530

    update

--- index.js
+++ index.js
@@ -22,15 +22,15 @@ dotenv.config({
 
 const app = express();
 
-// app.use(cors({
-//     // origin: process.env.CORS_ORIGIN,
-//     // credentials: true
+app.use(cors({
+    // origin: process.env.CORS_ORIGIN,
+    // credentials: true
 
     
-//         origin: 'http:0.0.0.0',
-//         credentials: true, 
+        origin: '*',
+        credentials: true, 
       
-// }));
+}));
 
 app.use(cookieParser());
 app.use(express.json({limit: "16kb"}));
